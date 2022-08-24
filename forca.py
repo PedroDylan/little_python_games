44734d9f97b7130730033f739698b87efd7c713f
pedro <pedrodylan3@gmail.com>
2022-08-24 19:05:14 -0300

    criando o game loop e mostrando as letras da palavra

--- forca.py
+++ forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = 'banana'
+    acertou = False
+    enforcou = False
+
+    while(not enforcou and not acertou):
+        chute = input('qual letra?: ')
+
+        index = 0
+        
+        for letra in palavra_secreta:
+            if(letra == chute ):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index = index + 1
+
+
+
+        print('Jogando...')
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
