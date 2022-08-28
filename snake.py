2dd473b3c926e7b047f4fc7e8068a9cdaac2cc4e
pedro <pedrodylan3@gmail.com>
2022-08-28 14:05:06 -0300

    criando funcionalidade de game over e aumentando o jogador com as frutas

--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
         self.direction = Vector2(1,0)
+        self.new_block = False
 
     def draw_snake(self):
         for block in self.body:
@@ -14,13 +15,23 @@ class SNAKE:
             pygame.draw.rect(screen,(183,111,122),block_rect)
 
     def move_snake(self):
-        #creating a copy of every element of the body 
-        #except for the last one
-        body_copy = self.body[:-1]
-        #inserting the head in the new body
-        body_copy.insert(0,body_copy[0]+self.direction)
-        #inserting the copy onto the actual drawn body
-        self.body = body_copy
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0,body_copy[0]+self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            #creating a copy of every element of the body 
+            #except for the last one
+            body_copy = self.body[:-1]
+            #inserting the head in the new body
+            body_copy.insert(0,body_copy[0]+self.direction)
+            #inserting the copy onto the actual drawn body
+            self.body = body_copy[:]
+
+
+    def add_block(self):
+        self.new_block = True
 
 class FRUIT:
     def __init__(self):
@@ -35,7 +46,6 @@ class FRUIT:
         self.y=random.randint(0,cell_number-1)
         self.pos = Vector2(self.x, self.y)
 
-
 class MAIN:
     def __init__(self):
         self.snake = SNAKE()
@@ -44,6 +54,7 @@ class MAIN:
     def update(self):
         self.snake.move_snake()
         self.check_collision()
+        self.check_fail()
 
     def draw_elements(self):
         self.fruit.draw_fruit()
@@ -52,6 +63,16 @@ class MAIN:
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
+            self.snake.add_block()
+
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
+
 
 pygame.init()
 cell_size = 30
