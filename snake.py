eccdf67c2240fca244973ac4b8bbe09b4aeb9f6b
pedro <pedrodylan3@gmail.com>
2022-08-28 13:31:30 -0300

    consertando bug na leitura de eventos e movendo o player

--- snake.py
+++ snake.py
@@ -4,6 +4,7 @@ from pygame.math import Vector2
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.direction = Vector2(1,0)
 
     def draw_snake(self):
         for block in self.body:
@@ -11,6 +12,16 @@ class SNAKE:
             y_pos = int(block.y*cell_size)
             block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
             pygame.draw.rect(screen,(183,111,122),block_rect)
+
+    def move_snake(self):
+        #creating a copy of every element of the body 
+        #except for the last one
+        body_copy = self.body[:-1]
+        #inserting the head in the new body
+        body_copy.insert(0,body_copy[0]+self.direction)
+        #inserting the copy onto the actual drawn body
+        self.body = body_copy
+
 
 
 
@@ -39,19 +50,25 @@ fruit = FRUIT()
 snake = SNAKE()
 
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
+
+
 #game loop that closes when the user closes it
 while True:
-    for event in pygame.event.get():
-        for event in pygame.event.get(): 
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == SCREEN_UPDATE:
+                snake.move_snake()
+
+            
 
 
-#filling the surfaces with color
-    
-    screen.fill((175,250,70))
-    fruit.draw_fruit()
-    snake.draw_snake()
-    pygame.display.update()
-    clock.tick(60)
+    #filling the surfaces with color
+        screen.fill((175,250,70))
+        fruit.draw_fruit()
+        snake.draw_snake()
+        pygame.display.update()
+        clock.tick(60)
