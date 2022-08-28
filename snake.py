97a37c8a6c69b6f452026525ab6405e28e1e01f0
pedro <pedrodylan3@gmail.com>
2022-08-28 00:17:49 -0300

    criando a classe fruta e respectivamente criando uam fruta em posição aleatória

--- snake.py
+++ snake.py
@@ -1,14 +1,28 @@
-import pygame, sys 
+import pygame, sys, random
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x=random.randint(0,cell_number-1)
+        self.y=random.randint(0,cell_number-1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x*cell_size,self.pos.y*cell_size,cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
+
 
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 
 #defining a clock to control the FPS
 #in order to the game run equally in every machine
 clock = pygame.time.Clock()
 
-#creating a surface in the background
-test_surface = pygame.Surface((100,200))
+fruit = FRUIT()
+
 
 #game loop that closes when the user closes it
 while True:
@@ -18,9 +32,10 @@ while True:
                 pygame.quit()
                 sys.exit()
 
-    test_surface.fill((50,50,250))
-    screen.fill((175,250,70))
 
-    screen.blit(test_surface,(200,250))
+#filling the surfaces with color
+    
+    screen.fill((175,250,70))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
